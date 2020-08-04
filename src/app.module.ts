7cac12be64ab8e6eed06e8d499e220f43f27da77
Danilo Rodrigues Catapan <dcatapan@gmail.com>
2020-08-03 23:13:23 -0300

    add MongooseModule configurations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
+import { PlayerSchema } from './players/player.schema';
+import { CategorySchema } from './categories/category.schema';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://admin:admin123@cluster0-wyrd1.mongodb.net/sradmbackend?retryWrites=true&w=majority',
+      {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+      },
+    ),
+    MongooseModule.forFeature([
+      { name: 'Category', schema: CategorySchema },
+      { name: 'Player', schema: PlayerSchema },
+    ])
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
