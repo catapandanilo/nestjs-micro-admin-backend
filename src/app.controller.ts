c9c9d4bec045b02f952ae74a81adf1776f6609a8
Danilo Rodrigues Catapan <dcatapan@gmail.com>
2020-08-11 23:27:01 -0300

    add controller and service getCategories

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,19 +1,27 @@
 import { Controller, Logger } from '@nestjs/common';
 import { AppService } from './app.service';
-import { EventPattern, Payload } from '@nestjs/microservices';
+import { EventPattern, Payload, MessagePattern } from '@nestjs/microservices';
 import { Category } from '../src/interfaces/categories/category.interface';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   logger = new Logger(AppController.name);
 
   @EventPattern('create-category')
   async createCategory(@Payload() category: Category) {
-    this.logger.log(`category: ${JSON.stringify(category)}`);
-
-    await this.appService.createCategory(category);
+    this.logger.log(`EventPattern > create-category: ${JSON.stringify(category)}`);
+    this.appService.createCategory(category);
   }
 
+  @MessagePattern('get-categories')
+  async getCategories(@Payload() _id: string) {
+    this.logger.log('MessagePattern > ' + (_id ? `get-category id: ${_id}` : `get-categories`));
+    if (_id) {
+      return await this.appService.getCategoryById(_id);
+    } else {
+      return await this.appService.getAllCategories();
+    }
+  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,17 +1,15 @@
-import { Injectable, Logger} from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { RpcException } from '@nestjs/microservices';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Category } from './interfaces/categories/category.interface';
-import { Player } from './interfaces/players/player.interface';
 
 @Injectable()
 export class AppService {
 
   constructor(
-    @InjectModel('Category') private categoryModel: Model<Category>,
-    @InjectModel('Player') private playerModel: Model<Player>
-  ) {}
+    @InjectModel('Category') private categoryModel: Model<Category>
+  ) { }
 
   private readonly logger = new Logger(AppService.name);
 
@@ -22,6 +20,24 @@ export class AppService {
     } catch (error) {
       this.logger.error(`error: ${JSON.stringify(error.message)}`);
       throw new RpcException(error.message);
+    }
+  }
+
+  async getAllCategories(): Promise<Category[]> {
+    try {
+      return await this.categoryModel.find().exec();
+    } catch (error) {
+      this.logger.error(`error: ${JSON.stringify(error.message)}`)
+      throw new RpcException(error.message)
+    }
+  }
+
+  async getCategoryById(_id: string): Promise<Category> {
+    try {
+      return await this.categoryModel.findOne({ _id }).exec()
+    } catch (error) {
+      this.logger.error(`error: ${JSON.stringify(error.message)}`)
+      throw new RpcException(error.message)
     }
   }
 }
